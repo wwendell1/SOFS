e820b323a1936c7a78da95ef9ed7d0eb00e5ea36
wwendell1 <wendelcostaa@gmail.com>
2024-11-15 23:55:55 -0300

    att do rodapee

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,3 +104,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+
+// Carrega o footer
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        });
+});
